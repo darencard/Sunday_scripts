d9595b9029aba7c0c383aadb96323596ef82eb23
darencard <dcard@uta.edu>
2015-04-19 13:37:20 -0500

    Read the data in.

--- Sunday_pm_gapminder.R
+++ Sunday_pm_gapminder.R
@@ -1,3 +1,7 @@
 # This is a script to explore the gapminder dataset using R
 # Daren Card
 # dcard@uta.edu
+# April 19, 2015
+
+# Read in data as tab-delimited
+gap.in <- read.table("output/combined_gapMinder.tsv", sep = "\t")
